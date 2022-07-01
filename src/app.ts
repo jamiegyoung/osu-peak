09551d0c9dface709c16a0038849ce5df7f22bdb
JamTheBean <ojamieo@hotmail.co.uk>
2022-07-01 16:28:38 +0100

    feat(app): add rate limiting for /u/ route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
+import rateLimit from "express-rate-limit";
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 import * as users from "./routes/users.js";
 
-app.get("/u/:userId", users.getById);
+app.get("/u/:userId", limiter, users.getById);
 
 app.listen(7527, () => {
-  console.log('Listening on port 7527');
+  console.log("Listening on port 7527");
 });
